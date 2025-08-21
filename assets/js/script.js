5c8bd7e4d9c77b16bbfbf41613b247bc185cf5c1
Mohamed Jalloh <codingwithmbj@gmail.com>
2025-08-20 22:45:43 -0500

    updated links and nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,26 +38,96 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   menuBtn.addEventListener("click", () => {
-    let isOpened = false;
-    if (!isOpened) {
-      menuBtn.classList.toggle("opened");
-      isOpened = true;
-    } else {
-      menuBtn.classList.remove("opened");
-      isOpened = false;
-    }
+    menuBtn.classList.toggle("opened");
+    nav.classList.toggle("opened");
   });
 
-  let currentScreenPosition = 0;
-  let isScrolling = false;
+  let lastPositionY = window.scrollY;
+  let scrollTimeout;
 
-  function scrolling() {
-    console.log(currentScreenPosition);
-  }
+  window.addEventListener("scroll", () => {
+    const currentPositionY = window.scrollY;
 
-  const scrollingPage = isScrolling ? "scrolling" : "notScrolled";
+    if (currentPositionY < lastPositionY && currentPositionY >= 100) {
+      header.classList.add("scrollingUp");
+      clearTimeout(scrollTimeout);
+      scrollTimeout = setTimeout(() => {
+        header.classList.remove("scrollingUp");
+      }, 3000);
+    } else {
+      header.classList.remove("scrollingUp");
+    }
 
-  body.classList.add(scrollingPage);
+    if (currentPositionY >= 100) {
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
+    }
 
-  window.addEventListener("scroll", () => {});
+    lastPositionY = currentPositionY;
+  });
+
+  fetch("/assets/data/navLinks.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Fetched JSON data:", data);
+
+      // Access navLinks properly
+      let navLinks = data.Links[0].navLinks;
+      let socialLinks = data.Links[1].socialLinks;
+
+      navLinks.forEach((link) => {
+        const ul = document.getElementById("nav-ul");
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const icon = document.createElement("i");
+
+        li.className = "nav-li";
+        a.className = "nav-li-a";
+        a.href = link.href;
+        a.textContent = link.name;
+
+        if (link.icon) {
+          icon.className = link.icon;
+          a.prepend(icon);
+        }
+
+        li.appendChild(a);
+
+        ul.appendChild(li);
+      });
+
+      socialLinks.forEach((link) => {
+        const ul = document.getElementById("hero-social-ul");
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const span = document.createElement("span");
+        const icon = document.createElement("i");
+
+        li.className = "social-li";
+        a.className = "social-li-a";
+        span.className = `social-text`;
+        a.href = link.href;
+        span.textContent = link.name;
+
+        if (link.icon) {
+          icon.className = `${link.icon} social-icon `;
+
+          a.prepend(icon);
+        }
+
+        a.appendChild(span);
+        li.appendChild(a);
+
+        ul.appendChild(li);
+      });
+    })
+    .catch((error) => {
+      console.log("Error fetching JSON:", error);
+    });
 });
